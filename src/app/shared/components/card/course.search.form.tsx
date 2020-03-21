42b2f6f9e22efe5b2d42e70b48448365bf3abb4b
tjaygallery <tjaygallery@gmail.com>
2020-03-21 20:51:25 +0100

    course layout search form

--- src/app/shared/components/card/course.search.form.tsx
+++ src/app/shared/components/card/course.search.form.tsx
@@ -6,7 +6,10 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import { TextField } from "@material-ui/core";
 import Button from '@material-ui/core/Button';
-import { Clear, Search } from "@material-ui/icons";
+import { Clear, FavoriteBorder, Search } from "@material-ui/icons";
+import { Formik } from "formik";
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import { useState } from "react";
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
@@ -28,13 +31,21 @@ const useStyles = makeStyles((theme: Theme) =>
 	}),
 );
 
+interface FormValues {
+	title: string
+	category: string,
+	favorite: boolean
+}
+
+const initialValues: FormValues = {
+	title: '',
+	category: '',
+	favorite: false
+};
 
 const CourseSearchForm = () => {
 	const classes = useStyles();
-	const [age, setAge] = React.useState('');
-	const handleChange = (event: React.ChangeEvent<{value: unknown}>) => {
-		setAge(event.target.value as string);
-	};
+	const [isFavorite, setIsFavorite] = useState(false);
 
 	const categories = [
 		'REACT',
@@ -44,56 +55,99 @@ const CourseSearchForm = () => {
 		'TYPESCRIPT'
 	];
 
-	return (
-		<>
-			<form>
-				<TextField variant="outlined" label="Tytuł" size="small" className={classes.formControl} />
-				<FormControl size="small" variant="outlined" className={classes.formControl}>
-					<InputLabel>Kategoria</InputLabel>
-					<Select
-						value={age}
-						onChange={handleChange}
-						label="Kategoria"
-					>
-						<MenuItem value="">
-							<em>Brak</em>
-						</MenuItem>
-						{categories.map(value =>
-							<MenuItem value={value}>{value}</MenuItem>
-						)}
-					</Select>
-				</FormControl>
-				<FormControl size="small" variant="outlined" className={classes.formControl}>
-					<InputLabel>Ulubione</InputLabel>
-					<Select
-						value={age}
-						onChange={handleChange}
-						label="Ulubione"
-					>
-						<MenuItem value={0}>
-							<em>Tak</em>
-						</MenuItem>
-						<MenuItem value={1}>
-							<em>Nie</em>
-						</MenuItem>
-					</Select>
-				</FormControl>
-				<Button className={classes.formControl}
-					variant="outlined"
-					size="medium"
-					startIcon={<Search/>}
-					onSubmit={() => console.log('submit')}
-				>
-					SZUKAJ
-				</Button>
-				<Button className={classes.formControl}
-						variant="outlined"
-						size="medium"
-						startIcon={<Clear/>}
-				>
-					WYCZYŚĆ
-				</Button>
-			</form>
+	const onSubmit = (values: FormValues) => {
+		console.log(values)
+	};
+
+	return (<>
+			<Formik<FormValues>
+				initialValues={initialValues}
+				onSubmit={onSubmit}
+				render={({
+							 values,
+							 resetForm,
+							 handleChange,
+							 handleBlur,
+							 handleSubmit,
+							 setFieldValue
+						 }) => (
+					<form onSubmit={e => {
+						e.stopPropagation();
+						handleSubmit(e)
+					}}>
+						<TextField
+							className={classes.formControl}
+							variant="outlined"
+							label="Tytuł"
+							size="small"
+							name="title"
+							onChange={handleChange}
+							onBlur={handleBlur}
+							value={values.title}
+						/>
+						<FormControl size="small" variant="outlined" className={classes.formControl}>
+							<InputLabel>Kategoria</InputLabel>
+							<Select
+								name="category"
+								value={values.category || ''}
+								onChange={handleChange}
+								label="Kategoria"
+							>
+								<MenuItem value="">
+									<em>Brak</em>
+								</MenuItem>
+								{categories.map(category =>
+									<MenuItem key={category} value={category}>{category}</MenuItem>
+								)}
+							</Select>
+						</FormControl>
+						{isFavorite ?
+							<Button className={classes.formControl}
+									size="medium"
+									variant="outlined"
+									onClick={() => {
+										setIsFavorite(!isFavorite);
+										setFieldValue('favorite', isFavorite)
+									}}
+									startIcon={<FavoriteBorder color="action"/>
+									}>
+								ULUBIONE
+							</Button>
+							:
+							<Button className={classes.formControl}
+									size="medium"
+									variant="outlined"
+									onClick={() => {
+										setIsFavorite(!isFavorite);
+										setFieldValue('favorite', isFavorite)
+									}}
+									startIcon={<FavoriteIcon color="action"/>
+									}>
+								ULUBIONE
+							</Button>}
+						<Button className={classes.formControl}
+								variant="outlined"
+								size="medium"
+								startIcon={<Search/>}
+								type="submit"
+						>
+							SZUKAJ
+						</Button>
+						<Button className={classes.formControl}
+								variant="outlined"
+								size="medium"
+								onClick={() => {
+									resetForm();
+									setIsFavorite(true)
+								}}
+								startIcon={<Clear/>}
+						>
+							WYCZYŚĆ
+						</Button>
+					</form>
+				)}
+			/>
+
 		</>
 	);
 };
